4f2b3736cbb3165cda09f7ed76bd99e6e00b5368
Shaker Yousef <shakrshrfa@gmail.com>
2024-05-07 23:38:53 +0300

    first commit

---
+++ Task1/Controllers/EmployeeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Task1.Data;
+
+namespace Task1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+        public IActionResult Index()
+        {
+            var employees = context.Employees.ToList();
+            return View("Index", employees);
+        }
+    }
+}
---
+++ Task1/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Task1.Models;
+
+namespace Task1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=.;database=Employee;trusted_connection=true;TrustServerCertificate=true");
+        }
+    }
+}
